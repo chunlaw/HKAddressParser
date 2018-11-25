6e32256c7109afc641c0ec9371906758c8a809e0
Nandiheath <nandi0315@gmail.com>
2018-11-25 13:51:06 +0800

    Fix an error for newer vue-cli version
    And also use the package-lock.json to enforce versioning for the modules.

--- web/babel.config.js
+++ web/babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  // To allow commonjs module.exports and require syntax work probably
+  // Seems vue-cli 3.0.3 onwards there will be error if not specify this
+  // https://github.com/vuejs/vue-cli/issues/2675
+  sourceType: 'unambiguous'
 }
