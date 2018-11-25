ca6274c9d2a2b28619c28f49f71efc35474b7711
wingkwong <wingkwong.code@gmail.com>
2018-11-25 18:10:37 +0800

    added Search Filter

---
+++ web/src/components/SearchFilter.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-container>
+        <v-layout row wrap>
+            <v-flex xs12 sm4 md4>
+                <v-checkbox
+                    :label="`Region`"
+                    v-model="region"
+                ></v-checkbox>
+
+                <v-checkbox
+                    :label="`DC District`"
+                    v-model="dcDistrict"
+                ></v-checkbox>
+
+            </v-flex>
+            <v-flex xs12 sm4 md4>
+                <v-checkbox
+                    :label="`Building Number from`"
+                    v-model="buildingNoFrom"
+                ></v-checkbox>
+
+                <v-checkbox
+                    :label="`Building Name`"
+                    v-model="buildingName"
+                ></v-checkbox>
+            </v-flex>
+            <v-flex xs12 sm4 md4>
+                <v-checkbox
+                    :label="`Street Name`"
+                    v-model="streetName"
+                ></v-checkbox>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+  props: {},
+  data: () => ({
+      region: true,
+      dcDistrict: true,
+      buildingNoFrom: true,
+      buildingName: true,
+      streetName: true
+  }),
+  methods: {
+      getFilterOptions: function() {
+          return {
+              region: this.region,
+              dcDistrict: this.dcDistrict,
+              buildingNoFrom: this.buildingNoFrom,
+              buildingName: this.buildingName,
+              streetName: this.streetName
+          }
+      }
+  }
+};
+</script>
