7aa94fce39779661c2e6c36c7a83549a951c7722
wingkwong <wingkwong.code@gmail.com>
2018-11-25 18:11:22 +0800

    filtered out certain fields based on input

--- web/src/components/SingleMatch.vue
+++ web/src/components/SingleMatch.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-expansion-panel-content>
+  <v-expansion-panel-content :disabled="disableContent">
     <div slot="header">
       <h3 class="headline mb-0">{{ fullChineseAddressFromResult(result.chi) }}</h3>
       <span class="pt-2">{{ rank === 0 ? 'BEST MATCH!' : `Rank #${rank + 1}` }}</span>
@@ -11,11 +11,12 @@
       >{{ result.score }}</v-chip><br>
       <span class="pt-2 grey--text">{{ result.geo.Latitude + "," + result.geo.Longitude }}</span>
     </div>
-    <v-card >
+    <v-card>
       <v-card-title primary-title>
         <v-container grid-list-md text-xs-left>
             <v-layout row wrap v-for="(value, key, index) in result.chi" :key="index"
                 :class="(index % 2 === 0 ? 'row-even': 'row-odd') + (isMatch(key)? ' matched': '')"
+                v-if="filterOptions[massageKey(key)]"
             >
               <v-flex class="field-title" xs4>{{ key }}</v-flex>
               <v-flex xs8>{{ value }}</v-flex>
@@ -40,16 +41,36 @@ export default {
       chi: Object,
       eng: Object,
       matches: Array
+    },
+    filterOptions: {
+      region: Boolean,
+      dcDistrict: Boolean,
+      buildingNoFrom: Boolean,
+      buildingName: Boolean,
+      streetName: Boolean
     }
   },
-  data: () => ({}),
+  data: () => ({
+    disableContent: false
+  }),
+  mounted: function () {
+    this.disableExpansionPanelContent();
+  },
   methods: {
     levelToString: utils.levelToString,
     fullChineseAddressFromResult: utils.fullChineseAddressFromResult,
     isMatch: function (key) {
       return this.result.matches.indexOf(key) >= 0;
+    },
+    massageKey: function (key) {
+      return key.charAt(0).toLowerCase() + key.slice(1);
+    },
+    disableExpansionPanelContent: function () {
+      const filterOptions = this.filterOptions;
+      if(Object.keys(filterOptions).every((key) => !filterOptions[key])) {
+          this.disableContent = true;
+      }
     }
-
   }
 };
 </script>
