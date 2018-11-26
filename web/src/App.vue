c2183d37818da139090b6175da62eb2f03c3c3b7
cswbrian <brianleung1017@gmail.com>
2018-11-26 19:28:34 +0800

    update google form link

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,9 +4,9 @@
       <v-toolbar-title>香港地址編碼定位器 Hong Kong Address Parser</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat to="/">進階處理</v-btn>
+        <v-btn flat to="/">單次處理</v-btn>
         <v-btn flat to="/batch">大量處理</v-btn>
-        <v-btn flat href="https://goo.gl/forms/lDKpoOm7bStvJ2483" target="_blank">反映意見</v-btn>
+        <v-btn flat href="https://goo.gl/forms/r6bdJHG228IZTgIZ2" target="_blank">反映意見</v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
