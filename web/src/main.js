55e9925fc3aaefa856dd73f05a4359af79264418
coolsunwind <brianleung1017@gmail.com>
2018-12-15 18:58:51 +0800

    vuelayer

--- web/src/main.js
+++ web/src/main.js
@@ -5,10 +5,12 @@ import './plugins/awesome-icon'
 import router from './route'
 import App from './App.vue'
 import JsonExcel from 'vue-json-excel'
+import VueLayers from 'vuelayers'
+import 'vuelayers/lib/style.css'
 
 Vue.config.productionTip = false
 Vue.component('downloadExcel', JsonExcel)
-
+Vue.use(VueLayers)
 new Vue({
   render: h => h(App),
   router
