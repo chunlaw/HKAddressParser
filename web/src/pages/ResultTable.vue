67db78663ca460ba0990b956bd206154de92ef9c
wingkwong <wingkwong.code@gmail.com>
2019-01-05 13:12:45 +0800

    #62 added ResultTable

---
+++ web/src/pages/ResultTable.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-content style="padding: 0px">
+    <div slot="header">進階選項</div>
+    <SearchFilter :filterOptions.sync="filterOptions" />
+    <!-- When to show the datatable? Now showing it when ever there is data -->
+    <v-data-table
+    :headers="normalizedHeaders"
+    :items="normalizedResults"
+    :rows-per-page-items="[ 50, 100, 500, { 'text': '$vuetify.dataIterator.rowsPerPageAll', 'value': -1 } ]"
+    class="elevation-1"
+    >
+    <template slot="items" slot-scope="props">
+        <td v-for="(field, index) in props.item" :key="index">{{ field }}</td>
+    </template>
+    </v-data-table>
+  </v-content>
+</template>
+
+<script>
+import SearchFilter from "./../components/SearchFilter";
+
+export default {
+    components: {
+        SearchFilter
+    },
+    props: {
+        filterOptions: Array,
+        normalizedHeaders: Array,
+        normalizedResults: Array
+    },
+    data: {
+        filterOptions: [],
+    },
+    computed: {
+        hasError: function() {
+            return this.errorMessage !== null;
+        },
+        
+    }
+}
+</script>
