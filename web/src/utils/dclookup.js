782d76cedf246887e533ad47e9472eb182fc8c99
Brian Leung <brianleung1017@gmail.com>
2019-06-27 01:15:52 +0800

    Refactor dclookup.js to query multiple DCCA

--- web/src/utils/dclookup.js
+++ web/src/utils/dclookup.js
@@ -1,36 +1,53 @@
 import * as turf from "@turf/turf"
-import dcdata from "./../utils/dcdata-2015";
+import dcdata15 from "./../utils/dcdata-2015";
+import dcdata19 from "./../utils/dcdata-2019";
 
-const dcArea = turf.featureCollection(dcdata.features);
+const dcArea = turf.featureCollection(dcdata15.features);
 
+const dcSettings = [
+  {
+    year: '2015',
+    polygon: 'polygon',
+    data: dcdata15
+  },
+  {
+    year: '2019',
+    polygon: 'multiPolygon',
+    data: dcdata19
+  }
+]
+
+const dcAreas = dcSettings.map(dc => {
+  return {
+    ...dc,
+    geojson: turf.featureCollection(dc.data.features)
+  }
+})
 export default {
   /**
    * Return the district and subdistrict name
    */
   dcNameFromCoordinates (lat, lng) {
-    const point = turf.point([lng, lat]);
+    const result = {}
+    const point = turf.point([lng, lat])
 
-
-    for (const feature of dcArea.features) {
-      if (turf.booleanPointInPolygon(point, turf.polygon(feature.geometry.coordinates))) {
-        return {
-          code: feature.properties.CACODE,
-          cname: feature.properties.CNAME,
-          ename: feature.properties.ENAME,
-          district: feature.properties.DISTRICT_T,
-          csubdistrict: feature.properties.SUBDISTIRCT_T,
-          esubdistrict: feature.properties.SUBDISTIRCT_E,
+    dcAreas.forEach(dcArea => {
+      for (const feature of dcArea.geojson.features) {
+        const turfFeature = dcArea.polygon === 'polygon' ? turf.polygon(feature.geometry.coordinates) : turf.multiPolygon(feature.geometry.coordinates)
+        // console.log(turfFeature)
+        if (turf.booleanPointInPolygon(point, turfFeature)) {
+          result[dcArea.year] = {
+            code: feature.properties.CACODE,
+            cname: feature.properties.CNAME,
+            ename: feature.properties.ENAME,
+            district: feature.properties.DISTRICT_T,
+            csubdistrict: feature.properties.SUBDISTIRCT_T,
+            esubdistrict: feature.properties.SUBDISTIRCT_E,
+          }
         }
       }
-    }
-    return {
-      code: 'unknown',
-      cname: 'unknown',
-      ename: 'unknown',
-      district: 'unknown',
-      csubdistrict: 'unknown',
-      esubdistrict: 'unknown',
-    }
+    })
 
+    return result
   }
 }
